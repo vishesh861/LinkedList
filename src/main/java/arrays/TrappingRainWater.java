69ca8e7bb6a4e5e45636086cf73d9c9230e19cd8
vishesh861 <55624142+vishesh861@users.noreply.github.com>
2026-04-13 21:19:46 +0530

    Update TrappingRainWater.java

--- src/main/java/arrays/TrappingRainWater.java
+++ src/main/java/arrays/TrappingRainWater.java
@@ -2,31 +2,24 @@ package arrays;
 
 public class TrappingRainWater {
 
-    public int trap(int[] height) {
-        int total = 0; // Initialize total water trapped
-        int l = 0, r = height.length - 1; // Two pointers, left (l) and right (r)
-        int lmax = 0, rmax = height[r]; // Initialize max heights for left and right
-
-        while (l < r) {
-            if (height[l] <= height[r]) {
-                // If left height is less than or equal to right height
-                if (height[l] < lmax) {
-                    total += lmax - height[l]; // Water trapped on the left
-                } else {
-                    lmax = height[l]; // Update left max height
-                }
-                l++; // Move left pointer
-            } else {
-                // If right height is less than left height
-                if (height[r] < rmax) {
-                    total += rmax - height[r]; // Water trapped on the right
-                } else {
-                    rmax = height[r]; // Update right max height
-                }
-                r--; // Move right pointer
-            }
+    public int maxArea(int[] height) {
+       int left  = 0;
+       int right = height.length-1;
+       int maxVolume = 0;
+       while (left < right) {
+        int min = Integer.min(height[left], height[right]);
+        int waterArea = min * (right - left);
+        if (maxVolume < waterArea) {
+            maxVolume = waterArea;
         }
-        return total; // Return total water trapped
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+       }
+
+       return maxVolume;
     }
 
 }
